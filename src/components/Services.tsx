e2bfc6fa53faba748ff83a06c75ff24740b4f50c
Jesús Emanuel Dupre <103268961+emadupre@users.noreply.github.com>
2025-03-24 00:17:37 -0300

    BG Cards

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -11,7 +11,7 @@ const displayCards = [
     date: "Servicio Disponible",
     iconClassName: "text-accent",
     titleClassName: "text-accent",
-    className: "[grid-area:stack] hover:-translate-y-10 before:absolute before:w-[100%] before:outline-1 before:rounded-xl before:outline-border before:h-[100%] before:content-[''] before:bg-blend-overlay before:bg-background/50 grayscale-[100%] hover:before:opacity-0 before:transition-opacity before:duration:700 hover:grayscale-0 before:left-0 before:top-0",
+    className: "[grid-area:stack] hover:-translate-y-10 before:absolute before:w-[100%] before:outline-1 before:rounded-xl before:outline-border before:h-[100%] before:content-[''] before:bg-blend-overlay before:bg-background/25 grayscale-[100%] hover:before:opacity-0 before:transition-opacity before:duration:700 hover:grayscale-0 before:left-0 before:top-0",
   },
   {
     icon: <Code className="size-4 text-secondary" />,
@@ -20,7 +20,7 @@ const displayCards = [
     date: "Servicio Disponible",
     iconClassName: "text-secondary",
     titleClassName: "text-secondary",
-    className: "[grid-area:stack] translate-x-12 translate-y-10 hover:-translate-y-1 before:absolute before:w-[100%] before:outline-1 before:rounded-xl before:outline-border before:h-[100%] before:content-[''] before:bg-blend-overlay before:bg-background/50 grayscale-[100%] hover:before:opacity-0 before:transition-opacity before:duration:700 hover:grayscale-0 before:left-0 before:top-0",
+    className: "[grid-area:stack] translate-x-12 translate-y-10 hover:-translate-y-1 before:absolute before:w-[100%] before:outline-1 before:rounded-xl before:outline-border before:h-[100%] before:content-[''] before:bg-blend-overlay before:bg-background/25 grayscale-[100%] hover:before:opacity-0 before:transition-opacity before:duration:700 hover:grayscale-0 before:left-0 before:top-0",
   },
   {
     icon: <Smartphone className="size-4 text-primary" />,
