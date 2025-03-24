77aff05d754d8219f9eec3e1511c5f921645b048
Jesús Emanuel Dupre <103268961+emadupre@users.noreply.github.com>
2025-03-24 01:48:52 -0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,15 +13,15 @@ const Navbar = () => {
             <img 
               src="/lovable-uploads/02e787e4-edc1-4555-8e33-101cfc7faa03.png" 
               alt="Digitaliza-T Logo" 
-              className="h-8 w-auto mb-6 invert"
+              className="h-8 w-auto invert"
             />
           </div>
           
           <div className="hidden md:flex items-center space-x-8">
-            <a href="#inicio" className="text-gray-800 hover:text-accent transition-colors">Inicio</a>
-            <a href="#servicios" className="text-gray-800 hover:text-accent transition-colors">Servicios</a>
-            <a href="#nosotros" className="text-gray-800 hover:text-accent transition-colors">Nosotros</a>
-            <a href="#contacto" className="text-gray-800 hover:text-accent transition-colors">Contacto</a>
+            <a href="#inicio" className="text-white-800 hover:text-accent transition-colors">Inicio</a>
+            <a href="#servicios" className="text-white-800 hover:text-accent transition-colors">Servicios</a>
+            <a href="#nosotros" className="text-white-800 hover:text-accent transition-colors">Nosotros</a>
+            <a href="#contacto" className="text-white-800 hover:text-accent transition-colors">Contacto</a>
           </div>
 
           <div className="md:hidden flex items-center">
@@ -38,10 +38,10 @@ const Navbar = () => {
       {isOpen && (
         <div className="md:hidden glass">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <a href="#inicio" className="block px-3 py-2 text-gray-800 hover:text-accent transition-colors">Inicio</a>
-            <a href="#servicios" className="block px-3 py-2 text-gray-800 hover:text-accent transition-colors">Servicios</a>
-            <a href="#nosotros" className="block px-3 py-2 text-gray-800 hover:text-accent transition-colors">Nosotros</a>
-            <a href="#contacto" className="block px-3 py-2 text-gray-800 hover:text-accent transition-colors">Contacto</a>
+            <a href="#inicio" className="block px-3 py-2 text-white-800 hover:text-accent transition-colors">Inicio</a>
+            <a href="#servicios" className="block px-3 py-2 text-white-800 hover:text-accent transition-colors">Servicios</a>
+            <a href="#nosotros" className="block px-3 py-2 text-white-800 hover:text-accent transition-colors">Nosotros</a>
+            <a href="#contacto" className="block px-3 py-2 text-white-800 hover:text-accent transition-colors">Contacto</a>
           </div>
         </div>
       )}
